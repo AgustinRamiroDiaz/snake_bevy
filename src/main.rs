a24b12dd48cf5453bb459222d00088f448892fea
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2023-10-05 19:13:22 -0300

    Finish book
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,6 @@ fn main() {
     App::new().add_plugins((DefaultPlugins, HelloPlugin)).run();
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
-
 #[derive(Component)]
 struct Person;
 
@@ -18,18 +14,24 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("peq".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    query
-        .iter()
-        .map(|name| println!("hello {}", name.0))
-        .for_each(drop);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        query
+            .iter()
+            .map(|name| println!("hello {}", name.0))
+            .for_each(drop);
+    }
 }
 
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people)
-            .add_systems(Update, (hello_world, greet_people));
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people)
+            .add_systems(Update, greet_people);
     }
 }
