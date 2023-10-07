df25f0baa1b67ce10f7aa5e8d5030cb4bd69ae0c
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2023-10-07 17:40:10 -0300

    Clean
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,44 +6,6 @@ fn main() {
     App::new().add_plugins((DefaultPlugins, SnakePlugin)).run();
 }
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Pet;
-
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("peq".to_string())));
-    commands.spawn((Person, Name("peq".to_string())));
-    commands.spawn((Pet, Name("michi".to_string())));
-    commands.spawn((Name("no tengo nombre".to_string()),));
-}
-
-#[derive(Resource)]
-struct GreetTimer(Timer);
-
-fn greet_people(query: Query<&Name, With<Person>>, time: Res<Time>, mut timer: ResMut<GreetTimer>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        query
-            .iter()
-            .map(|name| println!("hello {}", name.0))
-            .for_each(drop);
-    }
-}
-
-struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_people)
-            .add_systems(Update, greet_people);
-    }
-}
-
 struct SnakePlugin;
 
 const SNAKE_TICK_SECONDS: f32 = 0.3;
@@ -126,30 +88,6 @@ struct Snake {
 #[derive(Component)]
 struct SnakeSegment;
 
-#[derive(Clone)]
-enum Direction {
-    Down,
-    Left,
-    Right,
-    Up,
-}
-
-impl Into<Vec3> for Direction {
-    fn into(self) -> Vec3 {
-        let x = match self {
-            Direction::Down => (0, -1),
-            Direction::Left => (-1, 0),
-            Direction::Right => (1, 0),
-            Direction::Up => (0, 1),
-        };
-
-        Vec2::new(x.0 as f32, x.1 as f32).extend(0.0)
-    }
-}
-
-#[derive(Resource)]
-struct SnakeTimer(Timer);
-
 fn tick(
     time: Res<Time>,
     mut timer: ResMut<SnakeTimer>,
@@ -175,3 +113,26 @@ fn tick(
 // }
 
 ///////////
+#[derive(Resource)]
+struct SnakeTimer(Timer);
+
+#[derive(Clone)]
+enum Direction {
+    Down,
+    Left,
+    Right,
+    Up,
+}
+
+impl Into<Vec3> for Direction {
+    fn into(self) -> Vec3 {
+        let x = match self {
+            Direction::Down => (0, -1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
+            Direction::Up => (0, 1),
+        };
+
+        Vec2::new(x.0 as f32, x.1 as f32).extend(0.0)
+    }
+}
